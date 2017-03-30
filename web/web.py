ad0100f650a5d3c95802042f82650e5141acc3ac
Piotr Szyma <thompson2908@gmail.com>
2017-03-30 17:30:24 +0200

    Web app folder structure

---
+++ web/web.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=False)
