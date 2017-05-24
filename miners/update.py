e7416f17df05f33c35b5791fa26723dcbf3ed63d
MonikaTworek <MonikaTworek@users.noreply.github.com>
2017-05-24 16:55:33 +0200

    update

--- miners/update.py
+++ miners/update.py
@@ -2,33 +2,35 @@ import zipfile
 import requests
 import io
 import os
-import urlopen
+from urllib.request import urlopen
+import filecmp
 
 przystanki = urlopen('http://geoportal.wroclaw.pl/www/pliki/KomunikacjaZbiorowa/SlupkiWspolrzedne.txt')
 data = przystanki.read()
-txt_str = str(data)
-lines = txt_str.split("\\n")
 
 r = requests.get('http://www.wroclaw.pl/open-data/opendata/rozklady/OtwartyWroclaw_rozklad_jazdy_GTFS.zip')
 z = zipfile.ZipFile(io.BytesIO(r.content))
 
 des_url = 'C:\\Users\\Public\\PYTHON'
-if os.path.exists(des_url) :
-    #TODO:porownaj
+if os.path.exists(des_url):
+    filecmp.clear_cache()
+    check=filecmp.cmp("C:\\Users\\Public\\PYTHON\\przystanki.txt", data, shallow=False)
+    if check:
+        print("the same")
+    else:
+        filename = "C:\\Users\\Public\\PYTHON\\przystanki.txt"
+        with open(filename, "r+") as f:
+            f.seek(0)
+            f.write(data)
+    z.extractall(des_url) #uwaga
+
     print("check")
 else:
     directory = os.path.dirname(des_url)
     os.mkdir(directory)
-    z.extract(des_url)
-    
-    fx = open(des_url, "w")
-    for line in lines:
-        fx.write(line + "\n")
-    fx.close()
-    
-#     TODO:
-# Traceback (most recent call last):
-# 200
-#   File "C:/Users/Mona/Desktop/pythonowate/omega-wroclaw-dev-mine-positions/miners/update.py", line 7, in <module>
-#     przystanki = urlopen('http://geoportal.wroclaw.pl/www/pliki/KomunikacjaZbiorowa/SlupkiWspolrzedne.txt')
-# TypeError: 'module' object is not callable
+    z.extractall(des_url)
+
+    filename = "C:\\Users\\Public\\PYTHON\\przystanki.txt"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, "w") as f:
+        f.write(data)
