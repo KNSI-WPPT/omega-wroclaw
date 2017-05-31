6925c807e344d32ad688023254d01ff6e665d26e
MonikaTworek <MonikaTworek@users.noreply.github.com>
2017-05-31 16:19:53 +0200

    working update

--- miners/update.py
+++ miners/update.py
@@ -2,35 +2,26 @@ import zipfile
 import requests
 import io
 import os
-from urllib.request import urlopen
-import filecmp
 
-przystanki = urlopen('http://geoportal.wroclaw.pl/www/pliki/KomunikacjaZbiorowa/SlupkiWspolrzedne.txt')
-data = przystanki.read()
+przystanki=requests.get('http://geoportal.wroclaw.pl/www/pliki/KomunikacjaZbiorowa/SlupkiWspolrzedne.txt')
+przystanki=przystanki.text
 
 r = requests.get('http://www.wroclaw.pl/open-data/opendata/rozklady/OtwartyWroclaw_rozklad_jazdy_GTFS.zip')
 z = zipfile.ZipFile(io.BytesIO(r.content))
 
 des_url = 'C:\\Users\\Public\\PYTHON'
+filename = "C:\\Users\\Public\\PYTHON\\przystanki.txt"
 if os.path.exists(des_url):
-    filecmp.clear_cache()
-    check=filecmp.cmp("C:\\Users\\Public\\PYTHON\\przystanki.txt", data, shallow=False)
-    if check:
-        print("the same")
-    else:
-        filename = "C:\\Users\\Public\\PYTHON\\przystanki.txt"
-        with open(filename, "r+") as f:
-            f.seek(0)
-            f.write(data)
+    
+    with open(filename, "w") as f:
+        f.write(przystanki)
     z.extractall(des_url) #uwaga
 
-    print("check")
 else:
     directory = os.path.dirname(des_url)
     os.mkdir(directory)
     z.extractall(des_url)
 
-    filename = "C:\\Users\\Public\\PYTHON\\przystanki.txt"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, "w") as f:
-        f.write(data)
+        f.write(przystanki)
